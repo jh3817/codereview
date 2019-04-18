7b644f5e3622d8d396d32a268156c7df12eeba8e
jh3817 <49701329+jh3817@users.noreply.github.com>
2019-04-18 10:23:49 +0900

    change the algorithm

--- project6/shopping.cc
+++ project6/shopping.cc
@@ -23,21 +23,13 @@ void Shopping::PrintList() {
 // Sort shopping_list_ Elements by Price
 // Apply Selection Sort algorithm
 void Shopping::SortByPrice() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
-  std::vector<Goods>::iterator min = shopping_list_.end();
   for (std::vector<Goods>::iterator i = shopping_list_.begin();
       i != shopping_list_.end()-1; i++) {
-    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
-      if (min == shopping_list_.end()) {
-        min = j;
-        continue;
-      }
-      if (min->GetPrice() > j->GetPrice()) {
-        min = j;
+    for (std::vector<Goods>::iterator j = i + 1; j != shopping_list_.end(); j++) {
+      if (i->GetPrice() > j->GetPrice()) {
+        std::iter_swap(i, j);
       }
     }
-    std::iter_swap(i, min);
-    min = shopping_list_.end();
   }
 }
 
